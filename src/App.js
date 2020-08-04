e1d1db33d583eef664099891f40d5ad8cece38f3
AkshayShewate <aksshewate779@gmail.com>
2020-08-04 14:46:11 +0530

    Added to Pract2 barnch file upload

--- src/App.js
+++ src/App.js
@@ -1,95 +1,20 @@
-import React, {Component} from 'react';
-import axios  from 'axios';
-import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-class App extends React.Component{
-      state = {
-        selectedFile : null
-      }
-
-      onFileChange = event => { 
-        this.setState({ selectedFile: event.target.files[0] });        
-      }; 
-       
-      onFileUpload = () => { 
-        const formData = new FormData(); 
-
-        formData.append( 
-          "myFile", 
-          this.state.selectedFile, 
-          this.state.selectedFile.name 
-        );        
-     
-        console.log(this.state.selectedFile); 
-       
-        axios.post("api/uploadfile", formData); 
-      }; 
-       
-      fileData = () => { 
-       
-        if (this.state.selectedFile) { 
-            
-          return ( 
-            <div> 
-              <h2>File Details:</h2> 
-              <p>File Name: {this.state.selectedFile.name}</p> 
-              <p>File Type: {this.state.selectedFile.type}</p> 
-              <p> 
-                Last Modified:{" "} 
-                {this.state.selectedFile.lastModifiedDate.toDateString()} 
-              </p> 
-            </div> 
-          ); 
-        } else { 
-          return ( 
-            <div> 
-              <br /> 
-              <h4>Choose before Pressing the Upload button</h4> 
-            </div> 
-          ); 
-        } 
-      }; 
-       
-      render() { 
-       
-        return ( 
-          <div> 
-              <h3> 
-                File Upload using React! 
-              </h3> 
-              <div> 
-                  <input type="file" onChange={this.onFileChange} /> 
-                  <button onClick={this.onFileUpload}> 
-                    Upload! 
-                  </button> 
-              </div> 
-            {this.fileData()} 
-          </div> 
-        ); 
-      } 
+import React, { Component } from "react";
+import Stopwatch from "./Stopwatch";
+import Countdown from "./Countdown";
 
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Timers Demo</div>
+        <div className="Timers">
+          <Stopwatch />
+          <Countdown />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
