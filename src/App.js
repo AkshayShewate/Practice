73c2a42f73c9168033029b9b9ce86fa16e2fc0a9
AkshayShewate <aksshewate779@gmail.com>
2020-08-06 21:47:22 +0530

    Added to Pract3 barnch crud app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React, { Component } from "react";
-import Stopwatch from "./Stopwatch";
-import Countdown from "./Countdown";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './stylesheet/styles.css';
+import { Home } from './components/Home';
+import {Addemployee} from './components/Addemployees'
+import { Editemployee } from './components/Editemployees';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-title">Timers Demo</div>
-        <div className="Timers">
-          <Stopwatch />
-          <Countdown />
-        </div>
-      </div>
-    );
-  }
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+ <BrowserRouter>
+    <GlobalProvider>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/add" component={Addemployee} exact />
+          <Route path="/edit/:id" component={Editemployee} exact />
+        </Switch>
+      </GlobalProvider>
+</BrowserRouter>
+    
+  );
 }
 
 export default App;
+
+
+
