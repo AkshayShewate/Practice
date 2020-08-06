6e1b9d0601f4810e6f8ee1e59959a21a084a3fee
AkshayShewate <aksshewate779@gmail.com>
2020-08-06 23:01:54 +0530

    Added to Pract4 async tarditional way file upload with axios

--- src/App.js
+++ src/App.js
@@ -1,29 +1,82 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './stylesheet/styles.css';
-import { Home } from './components/Home';
-import {Addemployee} from './components/Addemployees'
-import { Editemployee } from './components/Editemployees';
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
 
+const API_BASE = "http://localhost:5000"
 
-import { GlobalProvider } from './context/GlobalState';
+function submitForm(contentType, data, setResponse) {
+ axios({
+ url: `${API_BASE}/upload`,
+ method: 'POST',
+ data: data,
+ headers: {
+ 'Content-Type': contentType
+ }
+ }).then((response) => {
+ setResponse(response.data);
+ }).catch((error) => {
+ setResponse("error");
+ })
+}
 
 function App() {
-  return (
- <BrowserRouter>
-    <GlobalProvider>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/add" component={Addemployee} exact />
-          <Route path="/edit/:id" component={Editemployee} exact />
-        </Switch>
-      </GlobalProvider>
-</BrowserRouter>
-    
-  );
+ const [title, setTitle] = useState("");
+ const [file, setFile] = useState(null);
+ const [desc, setDesc] = useState("");
+
+ function uploadWithFormData(){
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("file", file);
+  formData.append("desc", desc);
+ 
+  submitForm("multipart/form-data", formData, (msg) => console.log(msg));
+  }
+
+  async function uploadWithJSON(){
+    const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+    });
+   
+    const data = {
+    title: title,
+    file: await toBase64(file),
+    desc: desc
+    }
+   
+    submitForm("application/json", data, (msg) => console.log(msg));
+    }
+
+ return (
+ <div className="App">
+ <h2>Upload Form</h2>
+ <form>
+ <label>
+ File Title
+ <input type="text" vaue={title} 
+ onChange={(e) => { setTitle(e.target.value )}} 
+ placeholder="Give a title to your upload" />
+ </label>
+
+ <label>
+ File
+ <input type="file" name="file" onChange={(e) => setFile(e.target.files[0])} />
+ </label>
+
+ <label>
+ Description
+ <textarea value={desc} onChange={(e) => setDesc(e.target.value)}></textarea>
+ </label>
+
+ <input type="button" value="Upload as Form" onClick={uploadWithFormData} />
+ <input type="button" value="Upload as JSON" onClick={uploadWithJSON}/>
+ </form>
+ </div>
+ );
 }
 
 export default App;
-
-
-
